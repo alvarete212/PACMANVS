411300892e2cb745413c57407e2580d167437769
Laura <lauritaninetyseven@gmail.com>
2017-10-16 12:32:37 +0200

    Sprites

--- pacman vs/js/pacman.js
+++ pacman vs/js/pacman.js
@@ -4,7 +4,12 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload(){
 
     game.load.image('map','assets/map.png');
-
+    game.load.image("pill", "assets/pill16.png");
+    game.load.image('tiles', 'assets/pacman-tiles.png');
+    game.load.spritesheet('pacman','assets/pacman.png',32,32);
+    game.load.spritesheet("ghosts", "assets/ghosts32.png", 32, 32);
+    game.load.tilemap('map', 'assets/pacman-map.json', null, Phaser.Tilemap.TILED_JSON);
+    
 }
 
 function create(){
