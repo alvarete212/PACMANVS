59653e5191c6fed0dc0051f0e3b4b30f07cc97cd
Laura <lauritaninetyseven@gmail.com>
2017-10-18 11:59:33 +0200

    Version prueba cache

--- pacman vs/js/pacman.js
+++ pacman vs/js/pacman.js
@@ -1,10 +1,86 @@
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(450, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var PacmanGame = function () {    
+    game.map = null;
+    game.layer = null;
+    
+    game.numDots = 0;
+    game.TOTAL_DOTS = 0;
+    game.score = 0;
+    game.scoreText = null;
+    
+    game.pacman = null; 
+    game.clyde = null;
+    game.pinky = null;
+    game.inky = null;
+    game.blinky = null;
+    game.isInkyOut = false;
+    game.isClydeOut = false;
+    game.ghosts = [];
 
+    game.safetile = 14;
+    game.gridsize = 16;       
+    game.threshold = 3;
+    
+    game.SPECIAL_TILES = [
+        { x: 12, y: 11 },
+        { x: 15, y: 11 },
+        { x: 12, y: 23 },
+        { x: 15, y: 23 }
+    ];
+    
+    game.TIME_MODES = [
+        {
+            mode: "scatter",
+            time: 7000
+        },
+        {
+            mode: "chase",
+            time: 20000
+        },
+        {
+            mode: "scatter",
+            time: 7000
+        },
+        {
+            mode: "chase",
+            time: 20000
+        },
+        {
+            mode: "scatter",
+            time: 5000
+        },
+        {
+            mode: "chase",
+            time: 20000
+        },
+        {
+            mode: "scatter",
+            time: 5000
+        },
+        {
+            mode: "chase",
+            time: -1 // -1 = infinite
+        }
+    ];
+    game.changeModeTimer = 0;
+    game.remainingTime = 0;
+    game.currentMode = 0;
+    game.isPaused = false;
+    game.FRIGHTENED_MODE_TIME = 7000;
+    
+    game.ORIGINAL_OVERFLOW_ERROR_ON = true;
+    game.DEBUG_ON = true;
+    
+    game.KEY_COOLING_DOWN_TIME = 250;
+    game.lastKeyPressed = 0;
+    
+    game.game = game;
+};
 function preload(){
 
-    game.load.image('map','assets/map.png');
     game.load.image("pill", "assets/pill16.png");
+    game.load.image("dot","assets/dot.png");
     game.load.image('tiles', 'assets/pacman-tiles.png');
     game.load.spritesheet('pacman','assets/pacman.png',32,32);
     game.load.spritesheet("ghosts", "assets/ghosts32.png", 32, 32);
@@ -14,8 +90,20 @@ function preload(){
 
 function create(){
 
-    game.add.sprite(0, 0, 'map');
+    game.map = game.add.tilemap('map');
+    game.map.addTilesetImage('pacman-tiles', 'tiles');
+   // game.add.sprite(0,0,'pacman');
+   game.layer = game.map.createLayer('Pacman');
 
+   game.dots = game.add.physicsGroup();
+   game.numDots = game.map.createFromTiles(7, game.safetile, 'dot', game.layer, game.dots);
+   game.TOTAL_DOTS = game.numDots;
+   
+   game.pills = game.add.physicsGroup();
+   game.numPills = game.map.createFromTiles(40, game.safetile, "pill", game.layer, game.pills);
+   //  El primer valor de la funcion createFromTiles corresponde a las posiciones del tilemap que se quieren cambiar por objetos, en este caso, la imagen "pill"
+   game.dots.setAll('x', 6, false, false, 1);
+   game.dots.setAll('y', 6, false, false, 1);
 }
 
 function update(){}
